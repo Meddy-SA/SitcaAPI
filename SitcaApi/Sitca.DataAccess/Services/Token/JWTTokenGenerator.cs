677779399d68f369a3bac5bb7c2fd64bec207376
leonardoillanez <Leonardo@live.com.ar>
2024-10-07 16:57:42 -0300

    fix model and token

--- SitcaApi/Sitca.DataAccess/Services/Token/JWTTokenGenerator.cs
+++ SitcaApi/Sitca.DataAccess/Services/Token/JWTTokenGenerator.cs
@@ -9,42 +9,41 @@ using Microsoft.IdentityModel.Tokens;
 
 namespace Sitca.DataAccess.Services.Token
 {
-	public class JWTTokenGenerator : IJWTTokenGenerator
-	{
-		private readonly IConfiguration _config;
+  public class JWTTokenGenerator : IJWTTokenGenerator
+  {
+    private readonly IConfiguration _config;
 
-		public JWTTokenGenerator(IConfiguration config)
-		{
-			_config = config;
+    public JWTTokenGenerator(IConfiguration config)
+    {
+      _config = config;
+    }
+    public string GenerateToken(IdentityUser user, IList<string> roles, IList<Claim> claims)
+    {
+      claims.Add(new Claim(JwtRegisteredClaimNames.GivenName, user.UserName));
+      claims.Add(new Claim(JwtRegisteredClaimNames.Email, user.Email));
 
-		}
-		public string GenerateToken(IdentityUser user, IList<string> roles, IList<Claim> claims)
-		{			
-			claims.Add(new Claim(JwtRegisteredClaimNames.GivenName, user.UserName));
-			claims.Add(new Claim(JwtRegisteredClaimNames.Email, user.Email));
+      foreach (var role in roles)
+      {
+        claims.Add(new Claim(ClaimTypes.Role, role));
+      }
 
-			foreach (var role in roles)
-			{
-				claims.Add(new Claim(ClaimTypes.Role, role));
-			}
+      var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Token:Key"]));
 
-			var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Token:Key"]));
+      var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
 
-			var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+      var tokenDescriptor = new SecurityTokenDescriptor
+      {
+        Subject = new ClaimsIdentity(claims),
+        Expires = DateTime.Now.AddDays(7),
+        SigningCredentials = creds,
+        Issuer = _config["Token:Issuer"],
+      };
 
-			var tokenDescriptor = new SecurityTokenDescriptor
-			{
-				Subject = new ClaimsIdentity(claims),
-				Expires = DateTime.Now.AddDays(7),
-				SigningCredentials = creds,
-				Issuer = _config["Token:Issuer"],
-			};
+      var tokenHandler = new JwtSecurityTokenHandler();
 
-			var tokenHandler = new JwtSecurityTokenHandler();
+      var token = tokenHandler.CreateToken(tokenDescriptor);
 
-			var token = tokenHandler.CreateToken(tokenDescriptor);
-
-			return tokenHandler.WriteToken(token);
-		}
-	}
+      return tokenHandler.WriteToken(token);
+    }
+  }
 }
